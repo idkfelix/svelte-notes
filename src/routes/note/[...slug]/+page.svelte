10bcfcd1592325831fc81a9fd84f945a895f775b
idkfelix <felix.coulthard@gmail.com>
2023-07-12 13:07:25 +1000

    title data

--- src/routes/note/[...slug]/+page.svelte
+++ src/routes/note/[...slug]/+page.svelte
@@ -4,6 +4,10 @@
       content: '' };
 </script>
 
+<svelte:head>
+  <title>{data.content?.data.fm.title}</title>
+</svelte:head>
+
 <div class="markdown-body p-5 m-10 rounded-xl">
   {@html data.content?.code}
 </div>
