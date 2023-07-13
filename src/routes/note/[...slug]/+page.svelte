e8e7338d1dffd30be66af9177b6a080b3e902566
idkfelix <felix.coulthard@gmail.com>
2023-07-13 12:14:22 +1000

    .

--- src/routes/note/[...slug]/+page.svelte
+++ src/routes/note/[...slug]/+page.svelte
@@ -2,7 +2,7 @@
   import './markdown.css'
   export let data: any
   $: content = data.content
-  const meta = data.content.data.fm
+  $: meta = data.content.data.fm
   
 </script>
 
@@ -16,6 +16,6 @@
 
 <div class="m-10">
   <div class="markdown-body p-5 rounded-xl bg-base-300">
-    {@html $content?.code}
+    {@html content?.code}
   </div>
 </div>
